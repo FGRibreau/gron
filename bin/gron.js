21b604799f44b4d3ddb2a6e864d76a816b4435fe
FG Ribreau <github@fgribreau.com>
2016-10-04 23:19:49 +0200

    fix(gron)

--- bin/gron.js
+++ bin/gron.js
@@ -6,9 +6,13 @@ const s = process.openStdin();
 let d = '';
 s.on('data', c => d += c).on('end', () => console.log(objectToGron(JSON.parse(d))));
 
+function preserveDotInKeyName(k){
+  return k.replace(/\./g, '__*__');
+}
+
 function objectToGron(objectOrArray) {
   const gron = {
-    json: objectOrArray
+    json: _.mapKeys(objectOrArray, (v, k) => preserveDotInKeyName(k))
   };
 
   let seenPath = {};
@@ -52,7 +56,7 @@ function line(k, v) {
 }
 
 function key(k) {
-  return k.replace(/(\.([0-9]+))/g, '[$2]');
+  return k.replace(/(\.([0-9]+))/g, '[$2]').replace(/\.(\w+)__\*__(\w+)/, '["$1.$2"]');
 }
 
 function val(v) {
